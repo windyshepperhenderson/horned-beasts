7f42e79431967d3afc1a9ae8c8de77165cde4349
RORY MCCONNELL <skunkbentley@hotmail.com>
2023-10-26 17:49:32 +0100

    added extra filters

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,21 @@ import SelectedBeast from "./Components/SelectedBeast";
 
 function App() {
   const [showSelectedBeast, setShowSelectedBeast] = useState(false);
+  const [selectedHorns, setSelectedHorns] = useState("All");
+  const [titleInput, setTitleInput] = useState("");
+  const [filterActivated, setFilterActivated] = useState(false);
+
+  function handleTitleInputChange(event) {
+    const input = event.target.value;
+    setTitleInput(input);
+
+    // Activate filtering when at least 3 characters are entered
+    if (input.length >= 3) {
+      setFilterActivated(true);
+    } else {
+      setFilterActivated(false);
+    }
+  }
 
   function handleShowSelectedBeast() {
     // !showSelectedBeast is the opposite of the current value of showSelectedBeast
@@ -15,15 +30,56 @@ function App() {
     setShowSelectedBeast(!showSelectedBeast);
   }
 
+  function handleHornsFilter(event) {
+    setSelectedHorns(event.target.value);
+  }
+
+  const filteredBeasts =
+    selectedHorns === "All"
+      ? beastListArr
+      : beastListArr.filter((beast) => beast.horns == selectedHorns);
+
+  const filteredBeastsWithTitle = filterActivated
+    ? filteredBeasts.filter((beast) =>
+        beast.title.toLowerCase().includes(titleInput.toLowerCase())
+      )
+    : filteredBeasts;
+
   return (
     <div className="App">
+      <div className="dropDownFilter">
+        <label htmlFor="hornFilter">Filter Beasties by Horns </label>
+        <select
+          name=""
+          id="hornFilter"
+          value={selectedHorns}
+          onChange={handleHornsFilter}
+        >
+          <option value="All">All</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="100">100</option>
+        </select>
+      </div>
+
+      <div className="titleInput">
+        <label htmlFor="titleInput">Search for a Beastie: </label>
+        <input
+          type="text"
+          id="titleInput"
+          value={titleInput}
+          onChange={handleTitleInputChange}
+        />
+      </div>
+
       {/* We are conditionally rendering Modal, based on whether showModal is true or false */}
       {showSelectedBeast && (
         <SelectedBeast handleShowSelectedBeast={handleShowSelectedBeast} />
       )}
 
       <Header />
-      <Gallery beastList={beastListArr} />
+      <Gallery beastList={filteredBeastsWithTitle} />
       <Footer />
     </div>
   );
